21d9d25c85c8a58c74e4c99289cebf4cdd9edb92
tiesto95 <bes-zaurbaev@yandex.ru>
2023-03-08 22:25:23 +0300

    add

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/test/<num_one>/<num_two>')
+def test(num_one, num_two):
+    return 'Результат сложения' + str(int(num_one) + int(num_two))
+
+
+@app.route('/string/<str_one>/<str_two>/<str_three>')
+def string(str_one, str_two, str_three):
+    result = str_one if len(str_one) >= len(str_two) else str_two
+    result = result if len(result) >= len(str_three) else str_three
+    return result
+
+if __name__ == '__main__':
+    app.run()
